88c000e612acc414256cce24908eff374ef0d34f
PersianArt <persitheme@yahoo.com>
2019-08-09 10:57:18 +0430

    add videojs

---
+++ src/components/player/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import videojs from 'video.js';
+
+class VideoPlayer extends React.Component {
+	componentDidMount() {
+		// instantiate Video.js
+		this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+			console.log('onPlayerReady', this)
+		});
+	}
+
+	// destroy player on unmount
+	componentWillUnmount() {
+		if (this.player) {
+			this.player.dispose()
+		}
+	}
+
+	// wrap the player in a div with a `data-vjs-player` attribute
+	// so videojs won't create additional wrapper in the DOM
+	// see https://github.com/videojs/video.js/pull/3856
+	render() {
+		return (
+			<div>
+				<div data-vjs-player>
+					<video ref={ node => this.videoNode = node } className="video-js"></video>
+				</div>
+			</div>
+		)
+	}
+}
+
+
+const videoJsOptions = {
+	autoplay: true,
+	controls: true,
+	sources: [{
+		src: '/path/to/video.mp4',
+		type: 'video/mp4'
+	}]
+}
+
+export default <VideoPlayer { ...videoJsOptions } />
